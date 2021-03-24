d01cc2e5e8a8201c174ec2a6771334a16671b806
Emma Seiberlich <es3ur@virginia.edu>
2021-03-24 20:21:06 +0000

    git commit

--- try1.R
+++ try1.R
@@ -1,1 +1,15 @@
 # hi I have literally no idea what's going on
+library(tidyverse)
+install.packages("tidytext")
+library(tidytext)
+library(ggwordcloud)
+library(ggwordcloud)
+install.packages("gutenbergr") 
+library(gutenbergr)
+#install.packages('textdata')
+library(textdata)
+
+
+
+
+
